55d6630f7380558cc86cbc5571f45caecb47b496
max <max.r.johnson2021@gmail.com>
2025-02-12 16:17:40 -0600

    Set up test infrastructure + some base tests
    pytest recognizes tests that follow the format specified in pytest.ini
    
    Minor changes also done to the models to allow for some null vals

---
+++ backend/pages/models/Instrument.py
@@ -0,0 +1,10 @@
+from django.db import models
+import uuid
+
+class Instrument(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    instrument = models.CharField(max_length=255)
+    class_name = models.CharField(max_length=255, null=True)
+    
+    def __str__(self):
+        return f"{self.instrument} - {self.class_name}"
