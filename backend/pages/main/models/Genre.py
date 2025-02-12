b216716b6e8868b6240276ffd81be489f9c39d29
max <max.r.johnson2021@gmail.com>
2025-02-12 16:59:36 -0600

    Fix reformat issue

--- backend/pages/main/models/Genre.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-import uuid
-
-class Genre(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    genre = models.CharField(max_length=255)
-    
-    def __str__(self):
-        return f"{self.genre}"
--- backend/pages/main/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .musician import Musician
-from .business import Business
-from .instrument import Instrument
-from .genre import Genre
