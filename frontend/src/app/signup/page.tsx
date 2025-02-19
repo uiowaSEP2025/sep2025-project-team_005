143c0a1034df7dfe4ca6ba9412668753200ab688
root <root@Marissa-Laptop>
2025-02-19 09:30:45 -0600

    Added logo to signup page

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,11 +1,13 @@
 import styles from "@/styles/Signup.module.css";
+import Image from "next/image";
 
 console.log(styles); // Debugging: Check if styles are loaded correctly
 
 export default function Signup() {
   return (
     <div className={styles.header}>
-      <h1 className={styles.title}>Sign Up</h1>
+        <Image src="/savvy.png" alt="Platform Logo" width={200} height={200} />
+        <h1 className={styles.title}>Sign Up</h1>
     </div>
   );
 }
