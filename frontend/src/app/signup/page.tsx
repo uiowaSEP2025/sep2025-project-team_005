bc542daf911b0dff622a60f38b066e82ec216eb8
root <root@Marissa-Laptop>
2025-02-19 09:15:27 -0600

    Added css styling module for signup page

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,8 +1,11 @@
+import styles from "@/styles/Signup.module.css";
+
+console.log(styles); // Debugging: Check if styles are loaded correctly
+
 export default function Signup() {
-    return (
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Sign Up</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.title}>Sign Up</h1>
+    </div>
+  );
+}
