ffe7db5ef5924d44ec6f2cae6e6daec8c9412161
kvanorny <kvanorny@uiowa.edu>
2025-02-21 12:43:15 -0600

    Added restriction to the profile page, access token and refresh token must exist to access page. The following files allow this function:
    authentication/urls.py - API call to views function to handle restriction
    
    authentication/view.py - view functon for handling restriction. Changes to Custom Token class allow for HTTP storage of tokens during local devolpment. JS/TS cannot detect cookies otherwise. Changes to LogoutView can be ignored, function is not working
    
    login/page.tsx - Added routing to temp profile page
    
    profile/page.tsx - Temp profile page to route to after login. useEffect is the function that restricts access if a session token is not present. handleLogout function is not working, will require additional efforts.

--- backend/pages/authentication/urls.py
+++ backend/pages/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from pages.authentication.view import CustomTokenObtainPairView, LogoutView
+from pages.authentication.view import CustomTokenObtainPairView, LogoutView, ProfileView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = 'authentication'
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
