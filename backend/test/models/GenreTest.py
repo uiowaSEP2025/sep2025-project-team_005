80a26d6f4de637156cbfc0ade77d10ff794f8612
max <max.r.johnson2021@gmail.com>
2025-02-26 18:38:09 -0600

    Add GenreTest model validation tests
    Note that the object must be created with objects.create, or it doesn't store in the database and is just a Python object.
    
    nullcontext() ensures that the following lines do NOT raise an exception, and fails if it does.
    
    full_clean() re-validates the object, so it will raise a ValidationError if the object's values violate the model validations

--- backend/test/models/GenreTest.py
+++ backend/test/models/GenreTest.py
@@ -1,10 +1,36 @@
 import pytest
 from pages.models import Genre
+from contextlib import nullcontext
+from django.core.exceptions import ValidationError
 
 class GenreTest:
 
     @pytest.mark.django_db
-    def createGenre(self):
-        genre = Genre(genre="pop")
+    def test_genre_creation(self):
+        genre = Genre.objects.create(genre="Pop")
 
         assert genre.id is not None
+        assert genre.genre == "Pop"
+
+    @pytest.mark.django_db
+    def test_genre_string_representation(self):
+        genre = Genre.objects.create(genre="Savvy Punk")
+
+        assert str(genre) == "Savvy Punk"
+    
+    @pytest.mark.django_db
+    def test_genre_max_length(self):
+        genre = Genre.objects.create(genre="1" * 255)
+        with nullcontext():
+            genre.full_clean()
+
+        genre.genre ="1" * 256
+        with pytest.raises(ValidationError):
+            genre.full_clean()
+
+
+    @pytest.mark.django_db
+    def test_genre_invalid_inputs(self):
+        with pytest.raises(ValidationError):
+            Genre(genre=None).full_clean()
+
