ac7d9088892ca66119b2c7e8ab1f8b3c3a12847b
max <max.r.johnson2021@gmail.com>
2025-02-26 20:20:18 -0600

    Alter database to handle empty values better
    Though null=True is being set, blank=True also needs to be set to allow forms to leave the fields blank. If only null=True is set, the database can hold null values, but Django won't allow objects to validate None values without blank=True to be set as well.
    
    Additionally, it is common practice to replace null=True on fields that can accept string values with blank=True and default=''

--- backend/pages/models/Business.py
+++ backend/pages/models/Business.py
@@ -7,7 +7,7 @@ class Business(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='businesses')
     
     business_name = models.CharField(max_length=255)
-    industry = models.CharField(max_length=255, null=True)
+    industry = models.CharField(max_length=255, default="", blank=True)
 
     def __str__(self):
         return self.business_name
