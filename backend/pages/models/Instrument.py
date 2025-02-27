5b7ba6a8765cd9852cf76d929f717c6ae8353b74
max <max.r.johnson2021@gmail.com>
2025-02-26 20:20:18 -0600

    Alter database to handle empty values better
    Though null=True is being set, blank=True also needs to be set to allow forms to leave the fields blank. If only null=True is set, the database can hold null values, but Django won't allow objects to validate None values without blank=True to be set as well.
    
    Additionally, it is common practice to replace null=True on fields that can accept string values with blank=True and default=''

--- backend/pages/models/Instrument.py
+++ backend/pages/models/Instrument.py
@@ -4,7 +4,7 @@ import uuid
 class Instrument(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     instrument = models.CharField(max_length=255)
-    class_name = models.CharField(max_length=255, null=True)
+    class_name = models.CharField(max_length=255, default="", blank=True)
     
     def __str__(self):
         return f"{self.instrument} - {self.class_name}"
