7eaba2c9fe2dee012af4b938c72cfc528af5969c
max <max.r.johnson2021@gmail.com>
2025-02-26 21:28:19 -0600

    Use fixtures and explicitly call model validation
    Django doesn't automatically validate an object when saving it, it only enforces database level validations like null=true and unique=True. Other validators (blank=true, minLength) are not enforced.
    
    Since we want to validate that the database is not adding empty names, we must explicitly validate the model
    
    Also note that in the uniqueness test for users, simply putting the fixture in the function definition will populate the database with the user.

--- backend/test/models/GenreTest.py
+++ backend/test/models/GenreTest.py
@@ -5,32 +5,42 @@ from django.core.exceptions import ValidationError
 
 class GenreTest:
 
+    @pytest.fixture
+    def create_genre(db):
+        genre = Genre.objects.create(genre="Savvy Punk")
+        genre.full_clean()
+        return genre
+
     @pytest.mark.django_db
     def test_genre_creation(self):
         genre = Genre.objects.create(genre="Pop")
+        genre.full_clean()
 
         assert genre.id is not None
         assert genre.genre == "Pop"
 
     @pytest.mark.django_db
-    def test_genre_string_representation(self):
-        genre = Genre.objects.create(genre="Savvy Punk")
-
+    def test_string_representation(self, create_genre):
+        genre = create_genre
         assert str(genre) == "Savvy Punk"
     
     @pytest.mark.django_db
-    def test_genre_max_length(self):
-        genre = Genre.objects.create(genre="1" * 255)
+    def test_genre_max_length(self, create_genre):
+        genre = create_genre
+        genre.genre = "1" * 255
         with nullcontext():
             genre.full_clean()
 
-        genre.genre ="1" * 256
+        genre.genre = "1" * 256
         with pytest.raises(ValidationError):
             genre.full_clean()
 
 
     @pytest.mark.django_db
-    def test_genre_invalid_inputs(self):
+    def test_genre_invalid_inputs(self, create_genre):
+        genre = create_genre
+
+        genre.genre = None
         with pytest.raises(ValidationError):
-            Genre(genre=None).full_clean()
+            genre.full_clean()
 
--- backend/test/models/UserTest.py
+++ backend/test/models/UserTest.py
@@ -6,46 +6,48 @@ from datetime import datetime
 
 class UserTest:
 
+    @pytest.fixture
+    def create_user(db):
+        user = User.objects.create_user(username="testuser", email="test@test.com", password="password123")
+        user.full_clean()
+        return user
+    
     ## Test User object creation
     @pytest.mark.django_db
     def test_create_user(self):
         user = User.objects.create_user(username="testuser", email="test@test.com", password="password123")
+        user.full_clean()
         assert user.id is not None
         assert user.username == "testuser"
         assert user.email == "test@test.com"
         assert user.password != "password123"       # Doesn't equal due to password hashing
         assert user.role == "musician"              # Default role
 
-    ## Test default assignment of role parameter
+    ## Test default assignment of parameters
     @pytest.mark.django_db
-    def test_default_role(self):
-        user = User.objects.create_user(username="testuser", email="test@test.com", password="password123")
+    def test_default_values(self, create_user):
+        user = create_user
         assert user.role == "musician"
+        assert user.rating == 0.0
     
     ## Check that stored password value doesn't match entered valued -- indicate password is hashed
     @pytest.mark.django_db
-    def test_password_encrypted(self):
-        user = User.objects.create_user(username="testuser", email="test@test.com", password="password123")
+    def test_password_encrypted(self, create_user):
+        user = create_user
         assert user.password != "password123"
         assert user.check_password("password123")
     
     ## Tested embedded function to ensure email uniqueness
     @pytest.mark.django_db
-    def test_email_uniqueness(self):
-        User.objects.create_user(username="testuser1", email="test@test.com", password="password123")
+    def test_email_uniqueness(self, create_user):
+        # Just including the fixture as a parameter means that it is in the database
         with pytest.raises(IntegrityError):
             User.objects.create_user(username="testuser2", email="test@test.com", password="password123")
     
-    ## Test default assignment of rating parameter
-    @pytest.mark.django_db
-    def test_default_rating(self):
-        user = User.objects.create_user(username="testuser", email="test@test.com", password="password123")
-        assert user.rating == 0.0
-    
     ## Test function to assign optional paramters after object instantiation 
     @pytest.mark.django_db
-    def test_set_rating(self):
-        user = User.objects.create_user(username="testuser", email="test@test.com", password="password123")
+    def test_set_rating(self, create_user):
+        user = create_user
         user.rating = 4.5
         user.save()
         user = User.objects.get(id=user.id)
@@ -53,13 +55,13 @@ class UserTest:
 
     ## Test username is saved as a string object
     @pytest.mark.django_db
-    def test_string_representation(self):
-        user = User.objects.create_user(username="testuser", email="test@test.com", password="password123")
+    def test_string_representation(self, create_user):
+        user = create_user
         assert str(user) == "testuser"
 
     ## Test that user creation time is not null, is a datetime object, and is time-zone aware
     @pytest.mark.django_db
-    def test_created_at(self):
-        user = User.objects.create_user(username="testuser", email="test@test.com", password="password123")
+    def test_created_at(self, create_user):
+        user = create_user
         assert user.created_at is not None
         assert isinstance(user.created_at, datetime)
