a7b9b73982bf69ae8af6a16830873c85ff9e2465
kwixm <kwixm@uiowa.edu>
2025-02-28 17:50:12 -0600

    Added Dockerfiles and Docker-compose file

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+    eslint: {
+      // Warning: Disabling ESLint during builds might hide issues you want to fix.
+      ignoreDuringBuilds: true,
+    },
+  };
+  
