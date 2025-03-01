415660575b70de4275c251ab16e01b43b537f5da
kwixm <kwixm@uiowa.edu>
2025-02-28 18:06:37 -0600

    Removed old K8s dashboard code

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-module.exports = {
-    eslint: {
-      // Warning: Disabling ESLint during builds might hide issues you want to fix.
-      ignoreDuringBuilds: true,
-    },
-  };
-  
