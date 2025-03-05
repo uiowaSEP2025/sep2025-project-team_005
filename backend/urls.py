7f72dd1feb94b06f1d7ba2db01f8198efa627d29
kvanorny <kvanorny@uiowa.edu>
2025-03-05 09:19:11 -0600

    Added routing to a users unique profile page following the url "/profile/discoverprofile/[username]" Username becomes a dynamic part of the url because the file tree organization in the from end.
    Frontend Jest tests for the discover and login page have also been added. Run the tests following the instruction below:
    cd frontend
    npm run test
    npm test -- --coverage

--- backend/urls.py
+++ backend/urls.py
@@ -5,5 +5,5 @@ from pages.views.discover_views import GetUsersView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("pages.authentication.urls", namespace="authentication")),
-    path("create/", GetUsersView.as_view(), name="get_users"),
+    path("discover/", GetUsersView.as_view(), name="get_users"),
 ]
