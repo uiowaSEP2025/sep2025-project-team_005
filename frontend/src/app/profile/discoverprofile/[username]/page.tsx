d2aaf94d9e858287f2ab7b7a0bd7fcb8951ce353
kvanorny <kvanorny@uiowa.edu>
2025-03-05 09:19:11 -0600

    Added routing to a users unique profile page following the url "/profile/discoverprofile/[username]" Username becomes a dynamic part of the url because the file tree organization in the from end.
    Frontend Jest tests for the discover and login page have also been added. Run the tests following the instruction below:
    cd frontend
    npm run test
    npm test -- --coverage

--- frontend/src/app/profile/discoverprofile/[username]/page.tsx
+++ frontend/src/app/profile/discoverprofile/[username]/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import { useParams } from 'next/navigation';
-import { useRouter } from "next/navigation";
-import { useAuth, useRequireAuth } from "@/context/ProfileContext";
 
 export default function DiscoverProfile() {
-    useRequireAuth();
-    
-    const router = useRouter();
-    const { profile, isLoading } = useAuth();
     const { username } = useParams();
 
     return (
