da245f2eeb1a112bb6556bf97e2bd60c68c7cbb1
kvanorny <kvanorny@uiowa.edu>
2025-03-04 19:20:12 -0600

    Profile search is functional at /discover, authentication requirement has been commented out for dev. This page will pull up the first 5 profiles in the db, and then load 5 more if the user clicks "Load More". Searching will dynamically query db and when you click on a user it will rout to "discoverprofile/username" which will be the future UI of another users profile.
    To use the code, you may have to run "npm i --save-dev @types/lodash.debounce" as lodash.debounce is not native to typescript

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v@uu-r6&4c0zefohg2l2%zv(ve+-%7lzx(wk6fzl2)r@1@v_g3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
