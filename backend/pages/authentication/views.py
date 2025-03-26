022ccad57ffa25be969785d489137fc444724896
mmiller152 <marissa-miller@uiowa.edu>
2025-03-25 21:39:40 -0500

    Added back musician serializer and musician-instrument serializer that was lost during conflict resolution.

--- backend/pages/authentication/views.py
+++ backend/pages/authentication/views.py
@@ -277,3 +277,34 @@ def signup(request):
         return Response({"message": "User created successfully", "id": user.id}, status=status.HTTP_201_CREATED)
     
     return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Serializer class for the intermediate model between musicians and instrumens
+class MusicianInstrumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MusicianInstrument
+        fields = ['instrument', 'years_played']
+
+
+# Serializer class for musicians
+class MusicianSerializer(serializers.ModelSerializer):
+    instruments = MusicianInstrumentSerializer(many=True, write_only=True)
+    genres = serializers.PrimaryKeyRelatedField(many=True, queryset=Genre.objects.all())
+
+    class Meta:
+        model = Musician
+        fields = ['user', 'stage_name', 'home_studio', 'genres', 'instruments']
+
+    def create(self, validated_data):
+        instruments_data = validated_data.pop('instruments')
+        musician = Musician.objects.create(**validated_data)
+
+        # Create MusicianInstrument entries
+        for instrument_data in instruments_data:
+            MusicianInstrument.objects.create(
+                musician=musician,
+                instrument=instrument_data['instrument'],
+                years_played=instrument_data['years_played']
+            )
+
+        return musician
