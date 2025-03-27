9ea047ed18e856bd77fc385a19329e48c1bca547
Max Johnson <max.r.johnson2021@gmail.com>
2025-03-27 18:38:55 -0500

    Squashed commits

--- backend/pages/models/User.py
+++ backend/pages/models/User.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinLengthValidator
 from django.db import models
 from pages.models.Follower import Follower
+from pages.models.Follower import Follower
 import uuid
 
 class User(AbstractUser):
