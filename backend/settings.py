6a3e74e70d5595b08635464d4ce5f98485a22e76
mmiller152 <marissa-miller@uiowa.edu>
2025-04-02 08:58:19 -0500

    Got backend portion of google login working. Had to create a custom pipeline to redirect to sign up if email does not exist as a user yet. Custom user model we are using makes it so that the default functionality of the social_auth pipeline (which would typically automatically create a new user if it does not yet exist with that email) does not work, so had to implement custom pipeline. This is not yet connected to the frontend, and some redirect URLs are temporary and will need to be changed later, are just currently there for testing purposes.

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import environ
 import os
+from pages.pipeline import redirect_to_signup_if_new
 
 # Set up .env communication
 env = environ.Env()
@@ -113,6 +114,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',    # For google login and 2FA
             ],
         },
     },
@@ -240,14 +242,10 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
+    'pages.pipeline.redirect_to_signup_if_new',
     'social_core.pipeline.user.user_details',
 )
 
 LOGIN_URL = 'social:begin'
-LOGIN_REDIRECT_URL = "/api/instruments/all/"  # Redirect after successful login ***NOTE: CHANGE THESE LATER!
+LOGIN_REDIRECT_URL = "/api/users/all/"  # Redirect after successful login ***NOTE: CHANGE THESE LATER!
 LOGOUT_REDIRECT_URL = "/"  # Redirect after logout
-SOCIAL_AUTH_NEW_USER_REDIRECT_URL = "/api/auth/signup/"  # Redirect to sign up if user does not yet exist
