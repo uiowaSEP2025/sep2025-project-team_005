b6abffa8016f12bf2ad84c96cfe2e9338fd51c4f
mmiller152 <marissa-miller@uiowa.edu>
2025-04-01 23:09:04 -0500

    Made and ran new migration for removing follower and preventing self follow. Modified backend settings to contain pipeline for google social auth and got google to redirect to signup page if that email is not associated with a user yet. Currently LOGIN_REDIRECT_URL reroutes to a temporary test url until I can determine how to integrate with JWT tokens to direct to logged in user's discover page. Ran npm install to update dependencies.

--- backend/settings.py
+++ backend/settings.py
@@ -79,7 +79,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites'
+    'django.contrib.sites',
     'pages',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -97,6 +97,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'urls'
@@ -227,12 +228,26 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("GOOGLE_CLIENT_ID")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile',]
 SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_PARAMS = {'access_type': 'online'}
+SOCIAL_AUTH_GOOGLE_OAUTH2_USE_UNIQUE_USER_ID = True
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',  # Add Google OAuth2 backend
     'django.contrib.auth.backends.ModelBackend',  # Default backend
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 LOGIN_URL = 'social:begin'
-LOGIN_REDIRECT_URL = "/"  # Redirect after successful login ***NOTE: CHANGE THESE LATER!
+LOGIN_REDIRECT_URL = "/api/instruments/all/"  # Redirect after successful login ***NOTE: CHANGE THESE LATER!
 LOGOUT_REDIRECT_URL = "/"  # Redirect after logout
+SOCIAL_AUTH_NEW_USER_REDIRECT_URL = "/api/auth/signup/"  # Redirect to sign up if user does not yet exist
