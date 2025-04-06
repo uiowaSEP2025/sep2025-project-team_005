5d566847761807332f78dd7271c4a2be0866acd0
kvanorny <kvanorny@uiowa.edu>
2025-04-06 08:46:47 -0500

    Attempt 1: fix s3 boto3 mocks to pass CI in github

--- backend/test/utils/UtilsTest.py
+++ backend/test/utils/UtilsTest.py
@@ -11,6 +11,14 @@ def file_type():
     return "image/jpeg"
 
 @pytest.fixture
+def video_key():
+    return "user_0000/test.mp4"
+
+@pytest.fixture
+def video_type():
+    return "video/mp4"
+
+@pytest.fixture
 def mock_get_bucket_name(mocker):
     mock = mocker.patch("pages.utils.s3_utils.get_bucket_name")
     mock.return_value = "mock-bucket"
@@ -26,6 +34,17 @@ def mock_get_s3_client(mocker):
     mock_boto_client.return_value = mock_s3_client
     
     return mock_s3_client
+
+@pytest.fixture(autouse=True)
+def mock_django_settings(monkeypatch):
+    monkeypatch.delenv("AWS_PROFILE", raising=False)
+    monkeypatch.setattr("pages.utils.s3_utils.settings", MagicMock(
+        AWS_ACCESS_KEY_ID="fake_id",
+        AWS_SECRET_ACCESS_KEY="fake_secret",
+        AWS_REGION="us-east-1",
+        AWS_IMAGE_BUCKET_NAME="mock-bucket",
+        AWS_VIDEO_BUCKET_NAME="mock-video-bucket"
+))
 
 def test_generate_s3_url(file_key, file_type, mock_get_s3_client, mock_get_bucket_name):
     s3_url = generate_s3_url(file_key, file_type)
