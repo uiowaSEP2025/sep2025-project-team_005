9d7cf3c279116bf96a4032f83eb674fc95f51dd9
mmiller152 <marissa-miller@uiowa.edu>
2025-04-08 20:04:53 -0500

    Modified the temporary redirect page (google-auth/page.tsx) to have a material UI loading circle for better visual appeal. Implemented pytests for googl_login view function. Updated package and package lock after installing materialUI dependencies.

--- frontend/src/app/[username]/page.tsx
+++ frontend/src/app/[username]/page.tsx
@@ -138,9 +138,6 @@ export default function DiscoverProfile() {
             // Call next-auth sign out function to end next-auth session (google login) and remove next-auth.session-token from cookies
             // Once done, redirect to login page
             signOut({ callbackUrl: '/login' });
-
-            // Redirect to login page
-            //router.push("/login");
         } 
         catch (error) {
             console.error("Logout failed", error);
