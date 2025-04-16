cf81cdbe1d7638f66a6205ba218a5a04d06bf74c
kwixm <kwixm@uiowa.edu>
2025-04-16 11:56:59 -0500

    Added chrome-options to environment.py 'before_all' method

--- backend/test/features/environment.py
+++ backend/test/features/environment.py
@@ -12,8 +12,10 @@ def before_all(context):
     
     chrome_options = Options()
     chrome_options.headless = True
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--remote-debugging-port=9222")
 
     if not os.getenv("CI"):
         context.temp_dir = tempfile.mkdtemp(prefix=str(uuid.uuid4()))
