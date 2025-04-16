432736023aaf22e09efa6a9164620412821e92f9
kvanorny <kvanorny@uiowa.edu>
2025-04-16 00:58:53 -0500

    Test: CI dummy value

--- backend/settings.py
+++ backend/settings.py
@@ -51,7 +51,7 @@ AWS_METADATA_BUCKET_NAME="savvy-note-metadata"
 
 # Stripe environment variables
 STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
-STRIPE_WEBHOOK_SECRET = env('STRIPE_WEBHOOK_SECRET')
+STRIPE_WEBHOOK_SECRET = env('STRIPE_WEBHOOK_SECRET', default='dummy_ci_value')
 
 # Use different prefixes for dev vs prod
 if env("DJANGO_ENV") == "production":
