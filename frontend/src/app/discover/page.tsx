5dd070c1fae447c57c0431ea89d781a6e491f649
kwixm <kwixm@uiowa.edu>
2025-04-18 10:50:58 -0500

    Hadn't saved file on last push

--- frontend/src/app/discover/page.tsx
+++ frontend/src/app/discover/page.tsx
@@ -80,7 +80,7 @@ export default function Discover() {
 
         const fetchGenres = async () => {
             try {
-                const response = await fetch(`${process.env.BACKEND}/api/genres/all/`, {
+                const response = await fetch(`${process.env.BACKEND_API}/api/genres/all/`, {
                     method: 'GET',
                     headers: {
                       'Content-Type': 'application/json',
@@ -194,7 +194,7 @@ export default function Discover() {
     const fetchUsers = async (query = "", selectedInstruments: string[], selectedGenres: string[], pageNum = 1) => {
         setLoading(true);
         try {
-            const response = await axios.get(`${process.env.BACKEND}/api/discover/`, {
+            const response = await axios.get(`${process.env.BACKEND_API}/api/discover/`, {
                 params: {
                     search: query,
                     instrument: selectedInstruments,
